0ef88204f11359296ea150dba667c945b34bd80f
tomonakar <tomohisa.nak@gmail.com>
2018-04-01 18:29:16 +0900

    timing translateアニメーションの実装

--- animation/App.js
+++ animation/App.js
@@ -9,23 +9,20 @@ import {
 
 export default class animations extends Component {
   state = {
-    animation: new Animated.Value(1)
+    animation: new Animated.Value(0)
   }
   startAnimation = () => {
     Animated.timing(this.state.animation, {
-      toValue: 0,
-      duration: 350
+      toValue: 300,
+      duration: 1500
     }).start(() => {
-      Animated.timing(this.state.animation, {
-        toValue: 1,
-        duration: 500
-      }).start()
+      this.state.animation.setValue(0)
     })
   }
 
   render() {
     const animatedStyles = {
-      opacity: this.state.animation
+      transform: [{ translateY: this.state.animation }]
     }
     return (
       <View style={styles.container}>
