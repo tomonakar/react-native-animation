b95c167aacd6f0f68d03092a3ce4812f1e19163c
tomonakar <tomohisa.nak@gmail.com>
2018-04-01 18:02:08 +0900

    timing animationを追加

--- animation/App.js
+++ animation/App.js
@@ -1,58 +1,51 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
-  Platform,
+  AppRegistry,
   StyleSheet,
-  Text,
-  View
-} from 'react-native';
+  View,
+  Animated,
+  TouchableWithoutFeedback
+} from 'react-native'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class animations extends Component {
+  state = {
+    animation: new Animated.Value(1)
+  }
+  startAnimation = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 0,
+      duration: 350
+    }).start(() => {
+      Animated.timing(this.state.animation, {
+        toValue: 1,
+        duration: 500
+      }).start()
+    })
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
   render() {
+    const animatedStyles = {
+      opacity: this.state.animation
+    }
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <TouchableWithoutFeedback onPress={this.startAnimation}>
+          <Animated.View style={[styles.box, animatedStyles]} />
+        </TouchableWithoutFeedback>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: 'tomato'
+  }
+})
