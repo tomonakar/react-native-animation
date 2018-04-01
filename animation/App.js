8b445d95b38b5c0eec551e6b8e109b0cb037db7f
tomonakar <tomohisa.nak@gmail.com>
2018-04-01 19:12:58 +0900

    widthとheightをtiming アニメーションで変更

--- animation/App.js
+++ animation/App.js
@@ -10,31 +10,26 @@ import {
 
 export default class animations extends Component {
   state = {
-    animation: new Animated.Value(1)
+    animation: new Animated.Value(150)
   }
   startAnimation = () => {
     Animated.timing(this.state.animation, {
-      toValue: -1,
+      toValue: 300,
       duration: 1500
     }).start(() => {
-      this.state.animation.setValue(1)
+      this.state.animation.setValue(150)
     })
   }
 
   render() {
     const animatedStyles = {
-      transform: [
-        {
-          scaleY: this.state.animation
-        }
-      ]
+      width: this.state.animation,
+      height: this.state.animation
     }
     return (
       <View style={styles.container}>
         <TouchableWithoutFeedback onPress={this.startAnimation}>
-          <Animated.View style={[styles.box, animatedStyles]}>
-            <Text>This side is top</Text>
-          </Animated.View>
+          <Animated.View style={[styles.box, animatedStyles]} />
         </TouchableWithoutFeedback>
       </View>
     )
@@ -48,8 +43,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   box: {
-    width: 150,
-    height: 150,
+    // width: 150,
+    // height: 150,
     backgroundColor: 'tomato'
   }
 })
