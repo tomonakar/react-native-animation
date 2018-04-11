53f6d9c179fdeef0d947543fdc67391759560405
tomonakar <tomohisa.nak@gmail.com>
2018-04-11 19:32:59 +0900

    パラメータ調整

--- animation/App.js
+++ animation/App.js
@@ -14,7 +14,7 @@ export default class animations extends Component {
   startAnimation = () => {
     Animated.timing(this.state.animation, {
       toValue: 0,
-      duration: 400
+      duration: 140
     }).start(() => {
       Animated.timing(this.state.animation, {
         toValue: 1,
